032ca42e8ee363789ed7b909dfe91c92ce5d0aec
G1tgud-00 <93236591+G1tgud-00@users.noreply.github.com>
2023-03-25 19:12:35 +0700

    Add files via upload

---
+++ UDP Server-Client/UDP Client/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Drawing.Text;
+
+namespace UDP_Client
+{
+    public partial class Form1 : Form
+    {
+        private Socket test2;
+        private EndPoint testIP;
+        private EndPoint testLocal;
+        private byte[] buffer;
+        private void MessageCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                byte[] recieve = new byte[1024];
+                recieve = (byte[])ar.AsyncState;
+                Encoding encoding = new UTF8Encoding();
+                string texts = encoding.GetString(recieve);
+                texts = texts.Replace("\0", String.Empty);
+                
+                textBox2.AppendText(texts);
+                textBox2.AppendText(Environment.NewLine);
+
+                test2.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref testIP, new AsyncCallback(MessageCallBack), buffer);
+            } catch (Exception ex) { }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            Encoding encoding = new UTF8Encoding();
+            byte[] send = new byte[1024];
+            send = encoding.GetBytes(textBox1.Text);
+            test2.Send(send);
+            textBox2.AppendText(textBox1.Text);
+            textBox2.AppendText(Environment.NewLine);
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                test2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                test2.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                //testLocal = new IPEndPoint(IPAddress.Any, 20000);
+                //test2.Bind(testLocal);
+                testIP = new IPEndPoint(IPAddress.Loopback, 20000);
+                test2.Connect(testIP);
+                buffer = new byte[1024];
+                test2.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref testIP, new AsyncCallback(MessageCallBack), buffer);
+                Encoding encoding = new UTF8Encoding();
+                test2.Send(encoding.GetBytes("Connected!"));
+                button1.Enabled = false;
+            } catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+    }
+}
---
+++ UDP Server-Client/UDP Server/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
+using System.Threading;
+
+namespace UDP_Server
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient test1;
+        private IPEndPoint testIP;
+        private IPEndPoint testLocal;
+        private byte[] buffer;
+        private Thread thr;
+        public class UdpState
+        {
+            public UdpClient u;
+            public IPEndPoint e;
+            public UdpState(UdpClient u, IPEndPoint e)
+            {
+                this.u = u;
+                this.e = e;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            thr = new Thread(receiver);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            Encoding encoding = new UTF8Encoding();
+            byte[] send = new byte[1024];
+            send = encoding.GetBytes(textBox1.Text);
+            test1.Send(send, send.Length, testLocal);
+
+            addText(textBox1.Text);
+            textBox1.Text = "";
+        }
+        delegate void SetTextCallback(string text);
+        private void addText(string text) 
+        {
+            textBox2.AppendText(text);
+            textBox2.AppendText(Environment.NewLine); 
+        }
+        private void receiver()
+        {
+            while (true)
+            {
+                    byte[] buffer = test1.Receive(ref testLocal);
+                    string msg = Encoding.UTF8.GetString(buffer);
+                    msg = msg.Replace("\0", String.Empty);
+
+
+                    if (this.textBox2.InvokeRequired)
+                    {
+                        SetTextCallback d = new SetTextCallback(addText);
+                        this.Invoke(d, new object[] { msg });
+                    }
+                    else
+                    {
+                        addText(msg);
+                    }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            testLocal = new IPEndPoint(IPAddress.Any, 20000);
+            test1 = new UdpClient(testLocal);
+
+            thr.Start();
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
